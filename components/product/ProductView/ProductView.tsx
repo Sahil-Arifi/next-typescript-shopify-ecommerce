da455fdf64c9e066866ca99c077b301a2c4a50a9
Sahil-Arifi <sahil.arifi2006@gmail.com>
2022-06-15 10:44:24 -0700

    added variant options and cart implementation

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,16 +1,42 @@
 import cn from "classnames";
-import { FC } from "react";
+import { FC, useState } from "react";
 import s from "./ProductView.module.css";
 import { Button, Container } from "@components/ui";
 import Image from "next/image";
 import { Product } from "@common/types/product";
-import ProductSlider from "../ProductSlider";
+import { ProductSlider, Swatch } from "@components/product";
+import { Choices, getVariant } from "../helpers";
+import { useUI } from "@components/ui/context";
 
 interface Props {
   product: Product;
 }
 
+type AvailableChoices = "color" | "size" | string;
+
+type Choices = {
+  [P in AvailableChoices]: string;
+};
+
 const ProductView: FC<Props> = ({ product }) => {
+  const [choices, setChoices] = useState<Choices>({});
+  const { openSidebar } = useUI();
+
+  const variant = getVariant(product, choices);
+
+  const addToCart = () => {
+    try {
+      const item = {
+        productId: String(product.id),
+        variantId: variant?.id,
+        variantOptions: variant?.options,
+      };
+
+      alert(JSON.stringify(item));
+      openSidebar();
+    } catch {}
+  };
+
   return (
     <Container>
       <div className={cn(s.root, "fit", "mb-5")}>
@@ -39,19 +65,40 @@ const ProductView: FC<Props> = ({ product }) => {
         </div>
         <div className={s.sidebar}>
           <section>
-            <div className="pb-4">
-              <h2 className="uppercase font-medium">Color</h2>
-              <div className="flex flex-row py-4">Variant Options Here!</div>
-            </div>
+            {product.options.map((option) => (
+              <div key={option.id} className="pb-4">
+                <h2 className="uppercase font-medium">{option.displayName}</h2>
+                <div className="flex flex-row py-4">
+                  {option.values.map((optValue) => {
+                    const activeChoice =
+                      choices[option.displayName.toLowerCase()];
+                    return (
+                      <Swatch
+                        key={`${option.id}-${optValue.label}`}
+                        label={optValue.label}
+                        color={optValue.hexColor}
+                        variant={option.displayName}
+                        active={optValue.label.toLowerCase() === activeChoice}
+                        onClick={() => {
+                          setChoices({
+                            ...choices,
+                            [option.displayName.toLowerCase()]:
+                              optValue.label.toLowerCase(),
+                          });
+                        }}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            ))}
+
             <div className="pb-14 break-words w-full max-w-xl text-lg">
               {product.description}
             </div>
           </section>
           <div>
-            <Button
-              className={s.button}
-              onClick={() => alert("adding to cart")}
-            >
+            <Button className={s.button} onClick={addToCart}>
               Add to Cart
             </Button>
           </div>
---
+++ components/product/Swatch/Swatch.tsx
@@ -0,0 +1,45 @@
+import { ProductVariantSortKeys } from "@framework/schema";
+import { FC } from "react";
+import s from "./Swatch.module.css";
+import { Check } from "@components/icons";
+import cn from "classnames";
+import { isDark } from "@lib/color";
+
+interface Props {
+  color?: string;
+  label?: string;
+  active?: boolean;
+  variant?: "size" | "color" | string;
+  onClick: () => void;
+}
+
+const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
+  label = label?.toLowerCase();
+  variant = variant?.toLowerCase();
+
+  const rootClassName = cn(s.root, {
+    [s.active]: active,
+    [s.color]: color,
+    [s.size]: variant === "size",
+    [s.dark]: color && isDark(color),
+  });
+
+  return (
+    <>
+      <button
+        style={color ? { backgroundColor: color } : {}}
+        className={rootClassName}
+        {...rest}
+      >
+        {variant === "color" && active && (
+          <span>
+            <Check />
+          </span>
+        )}
+        {variant === "size" ? label : null}
+      </button>
+    </>
+  );
+};
+
+export default Swatch;
